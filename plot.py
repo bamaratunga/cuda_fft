cd62128132b9a49f568339170e5ae03ed1d8f706
Binu Amaratunga <7069045+bamaratunga@users.noreply.github.com>
2021-06-29 08:36:25 +0530

    Modify

--- plot.py
+++ plot.py
@@ -5,8 +5,8 @@ from matplotlib import pyplot as plt
 
 
 data = genfromtxt('output_0.csv', delimiter=',')
-# plt.rcParams["figure.figsize"] = [7.00, 3.50]
-# plt.rcParams["figure.autolayout"] = True
+plt.rcParams["figure.figsize"] = [7.00, 3.50]
+plt.rcParams["figure.autolayout"] = True
 data = np.fft.fftshift(data)
 plt.imshow(data, extent=[-1, 1, -1, 1])
 plt.show()
